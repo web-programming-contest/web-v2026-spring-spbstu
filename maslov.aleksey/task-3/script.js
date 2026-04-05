4f057cf2383573c92c13be829b32bea9a69b9705
A1exMas1ov <ae1ex.mas1ov@gmail.com>
2026-04-05 22:24:15 +0300

    add func logic

--- maslov.aleksey/task-3/script.js
+++ maslov.aleksey/task-3/script.js
@@ -1,2 +1,14 @@
 function sortArrayByParity(arr) {
+    const even = [];
+    const odd = [];
+    for (let num of arr) {
+        if (num % 2 === 0) {
+            even.push(num);
+        } else {
+            odd.push(num);
+        }
+    }
+    return even.concat(odd);
 }
+
+console.log(sortArrayByParity([3, 1, 2, 4]));
