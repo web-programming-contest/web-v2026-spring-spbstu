2cfaf449f4bb86f02e762bf28e066cab6153adfe
A1exMas1ov <ae1ex.mas1ov@gmail.com>
2026-04-05 21:53:39 +0300

    add exceptions

--- maslov.aleksey/task-2/script.js
+++ maslov.aleksey/task-2/script.js
@@ -1,4 +1,11 @@
 function generateParenthesis(n) {
+    if (!Number.isInteger(n)) {
+        throw new Error("parameter n must be an int");
+    }
+    if (n < 0) {
+        throw new Error("parameter n cannot be negative");
+    }
+    if (n === 0) return [];
     const result = [];
     function buildStrings(currentString, openCount, closeCount) {
         if (currentString.length === n * 2) {
@@ -16,5 +23,18 @@ function generateParenthesis(n) {
     return result;
 }
 
-console.log("n = 2: ", generateParenthesis(2));
-console.log("n = 3: ", generateParenthesis(3));
+try {
+    console.log("n = 2: ", generateParenthesis(2));
+    console.log("n = 3: ", generateParenthesis(3));
+    console.log("n = 0: ", generateParenthesis(0));
+    console.log(generateParenthesis("abcde"));
+}
+catch(e) {
+    console.error("Error: " + e.message)
+}
+try {
+    console.log("n = -1: ", generateParenthesis(-1));   
+}
+catch(e) {
+    console.error("Error: " + e.message)
+}
