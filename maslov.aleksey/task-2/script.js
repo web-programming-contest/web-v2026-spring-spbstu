3dbe58e1664912cfe991f578c8e39de954303bc3
A1exMas1ov <ae1ex.mas1ov@gmail.com>
2026-04-05 21:41:37 +0300

    add func logic

--- maslov.aleksey/task-2/script.js
+++ maslov.aleksey/task-2/script.js
@@ -1,4 +1,19 @@
 function generateParenthesis(n) {
+    const result = [];
+    function buildStrings(currentString, openCount, closeCount) {
+        if (currentString.length === n * 2) {
+            result.push(currentString);
+            return;
+        }
+        if (openCount < n) {
+            buildStrings(currentString + "(", openCount + 1, closeCount);
+        }
+        if (closeCount < openCount) {
+            buildStrings(currentString + ")", openCount, closeCount + 1);
+        }
+    }
+    buildStrings("", 0, 0);
+    return result;
 }
 
 console.log("n = 2: ", generateParenthesis(2));
