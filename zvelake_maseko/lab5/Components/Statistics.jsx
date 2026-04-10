6d454c0cfc55ec0c8ef435532ce23dc94b627d48
Zwelakhe-git <zweklakhe.mzwet@gmail.com>
2026-04-10 19:39:47 +0300

    Refactor Statistics component and add TransactionCard

--- zvelake_maseko/lab5/Components/Statistics.jsx
+++ zvelake_maseko/lab5/Components/Statistics.jsx
@@ -1,24 +1,17 @@
-import React from "react";
-import Chart from "./Chart";
+import React from "react"
 
-export default function Statistics({
-    setCurrentSection, 
-    transactionsList
-}){
-    return (<div className="stats-section">
-        <div className="section-info">
-            <h1 className="section-title">Граф</h1>
-        </div>
-        <div className="section">
-            <div className="section-nav">
-                <ul className="section-nav-links">
-                    <li className="section-nav-links__item" onClick={()=>{setCurrentSection(0)}}>На Главную</li>
-                </ul>
-            </div>
-            <div className="section-body">
-                <div className="graph-box">
-                    <Chart data={transactionsList}/>
-                </div>
+export default function TransactionCard({transaction}){
+    return (<div className="transaction-item">
+        <div className="card-body">
+            <div className="transaction-info">
+                <span style={{fontWeight: 'bold'}}>{transaction.name}</span><br/>
+                <span>{(new Date(transaction.date)).toLocaleDateString('ru-Ru', {
+                            month: 'long',
+                            day: '2-digit',
+                        })}</span><br/>
+                <span>{transaction.amount}</span><br/>
+                <span>{transaction.paymentStatus}</span>
+                {transaction.paymentStatus !== 'PENDING' && transaction.paymentMethod}
             </div>
         </div>
     </div>)
