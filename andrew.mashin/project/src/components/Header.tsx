af0b678b18dffae30a76ca3c629d5c1e08a84228
Andrew <97516045+Exam-Play@users.noreply.github.com>
2026-04-11 20:59:38 +0300

    [PROJECT] Upgrade header

--- andrew.mashin/project/src/components/Header.tsx
+++ andrew.mashin/project/src/components/Header.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import catalogLogo from '../assets/images/icons/catalog.svg'
@@ -5,18 +6,30 @@ import profileLogo from '../assets/images/icons/profile.svg'
 
 import Title from './Title';
 
+
 function Header() {
+    const [active, setActive] = useState('home');
+
     return <header>
-        <Link to="/">
+        <Link to="/" onClick={() => setActive('home')}>
             <Title />
         </Link>
+
         <div className='items'>
-            <Link to="/catalog">
+            <Link
+                to="/catalog"
+                className={active === 'catalog' ? 'active' : ''}
+                onClick={() => setActive('catalog')}
+            >
                 <img src={catalogLogo} alt='catalog-logo'/>
                 <p>Каталог</p>
             </Link>
 
-            <Link to="/profile">
+            <Link
+                to="/profile"
+                className={active === 'profile' ? 'active' : ''}
+                onClick={() => setActive('profile')}
+            >
                 <img src={profileLogo} alt='profile-logo'/>
                 <p>Войти</p>
             </Link>
