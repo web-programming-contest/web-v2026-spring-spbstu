84ac4fe7b8d3eac21cd14ca55d47e192768bb847
InsomniaDemon <chelovefick@gmail.com>
2026-04-16 16:38:37 +0300

    [TASK-5] Chess working completely correct

--- kolosov.ivan/task-5/chess/src/utils/countAvailableMovesForKing.js
+++ kolosov.ivan/task-5/chess/src/utils/countAvailableMovesForKing.js
@@ -1,3 +1,5 @@
+import isSquareUnderAtack from "./isSquareUnderAtack";
+
 function countAvailableMovesForKing(row, col, board) {
     let theoreticallyPossibleMoves = [
         {row: row + 1, col: col + 1}, {row: row - 1, col: col - 1},
@@ -20,10 +22,10 @@ function countAvailableMovesForKing(row, col, board) {
         availableMoves.push(move);
     }
 
-    if (board[kingIndex].moveNumber === 0 && board[row * 8 + 7].moveNumber === 0 && board[row * 8 + 6].pieceType === "none" && board[row * 8 + 5].pieceType === "none") {
+    if (board[kingIndex].moveNumber === 0 && board[row * 8 + 7].moveNumber === 0 && board[row * 8 + 6].pieceType === "none" && board[row * 8 + 5].pieceType === "none" && !isSquareUnderAtack(row, 5, kingColour === "white" ? "black" : "white", board)) {
         availableMoves.push({row: row, col: 6});
     }
-    if (board[kingIndex].moveNumber === 0 && board[row * 8 + 0].moveNumber === 0 && board[row * 8 + 1].pieceType === "none" && board[row * 8 + 2].pieceType === "none" && board[row * 8 + 3].pieceType === "none") {
+    if (board[kingIndex].moveNumber === 0 && board[row * 8 + 0].moveNumber === 0 && board[row * 8 + 1].pieceType === "none" && board[row * 8 + 2].pieceType === "none" && board[row * 8 + 3].pieceType === "none" && !isSquareUnderAtack(row, 3, kingColour === "white" ? "black" : "white", board)) {
         availableMoves.push({row: row, col: 2});
     }
 
