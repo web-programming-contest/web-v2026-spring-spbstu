10e7c3109c3b69742a056173dd78c515ec339b8b
InsomniaDemon <chelovefick@gmail.com>
2026-04-16 16:29:46 +0300

    [TASK-5] Almost done

--- kolosov.ivan/task-5/chess/src/GameOfChess.jsx
+++ kolosov.ivan/task-5/chess/src/GameOfChess.jsx
@@ -1,5 +1,5 @@
 import Board from './Board.jsx'
-import {useState} from 'react'
+import { useState } from 'react'
 import startingBoard from './Board.json'
 import findIsInAvailableMovesValue from './utils/findIsInAvailableMovesValue.js';
 import countAvaiableMovesForKnight from './utils/countAvailableMovesForKnight.js';
@@ -8,6 +8,7 @@ import countAvaiableMovesForRook from './utils/countAvailableMovesForRook.js';
 import countAvaiableMovesForQueen from './utils/countAvailableMovesForQueen.js';
 import countAvaiableMovesForPawn from './utils/countAvailableMovesForPawn.js';
 import countAvaiableMovesForKing from './utils/countAvailableMovesForKing.js';
+import filterMovesLeavingKingInCheck from './utils/filterMovesLeavingKingInCheck.js';
 
 function GameOfChess() {
     const [board, setBoard] = useState(startingBoard);
@@ -17,6 +18,10 @@ function GameOfChess() {
     const [availableMoves, setAvailableMoves] = useState([]);
 
     const currentTurn = moveNumber % 2 === 1 ? "white" : "black";
+
+    if (board.every(position => (position.pieceColour === currentTurn && position.possibleMoves.length === 0) || position.pieceColour !== currentTurn)) {
+        console.log("GAME OVER");
+    }
 
     function handleSquareClick(row, col) {
         let destinationIndex = row * 8 + col;
@@ -41,12 +46,35 @@ function GameOfChess() {
                     board[destinationIndex].pieceType === "none") {
                     newBoard[selectedPosition.row * 8 + col].pieceColour = "none";
                     newBoard[selectedPosition.row * 8 + col].pieceType = "none";
-                    newBoard[selectedPosition.row * 8 + col].availableMoves = [];
+                    newBoard[selectedPosition.row * 8 + col].possibleMoves = [];
                     newBoard[selectedPosition.row * 8 + col].moveNumber = moveNumber;
+                }
+                
+                if (board[selectedIndex].pieceType === "king" && board[selectedIndex].moveNumber === 0 && selectedPosition.row === row) {
+                    if (col === 6) {    
+                        newBoard[row * 8 + 7].pieceColour = "none";
+                        newBoard[row * 8 + 7].pieceType = "none";
+                        newBoard[row * 8 + 7].possibleMoves = [];
+                        newBoard[row * 8 + 7].moveNumber = moveNumber;
+                        newBoard[row * 8 + 5].pieceColour = currentTurn;
+                        newBoard[row * 8 + 5].pieceType = "rook";
+                        newBoard[row * 8 + 5].possibleMoves = [];
+                        newBoard[row * 8 + 5].moveNumber = moveNumber;
+                    }
+                    if (col === 2) {    
+                        newBoard[row * 8].pieceColour = "none";
+                        newBoard[row * 8].pieceType = "none";
+                        newBoard[row * 8].possibleMoves = [];
+                        newBoard[row * 8].moveNumber = moveNumber;
+                        newBoard[row * 8 + 3].pieceColour = currentTurn;
+                        newBoard[row * 8 + 3].pieceType = "rook";
+                        newBoard[row * 8 + 3].possibleMoves = [];
+                        newBoard[row * 8 + 3].moveNumber = moveNumber;
+                    }
                 }
 
                 newBoard[destinationIndex] = newBoard[selectedIndex];
-                newBoard[selectedIndex] = { pieceColour: "none", pieceType: "none" };
+                newBoard[selectedIndex] = { pieceColour: "none", pieceType: "none", possibleMoves: []};
 
                 newBoard[destinationIndex].moveNumber = moveNumber;
                 newBoard[selectedIndex].moveNumber = moveNumber;
@@ -55,6 +83,9 @@ function GameOfChess() {
                         newBoard = countAvailableMoves(row, col, newBoard);
                     }
                 }
+                
+                console.log(newBoard);
+
                 setBoard(newBoard);
                 setMoveNumber(moveNumber + 1);
                 setSelectedPosition(null);
@@ -64,23 +95,23 @@ function GameOfChess() {
     }
 
     function countAvailableMoves(row, col, board) {
-        const pieceType = board[row * 8 + col].pieceType;
+        const selectedIndex = row * 8 + col;
+        const pieceType = board[selectedIndex].pieceType;
+        const pieceColour = board[selectedIndex].pieceColour;
+        let newBoard;
+
         switch (pieceType) {
-            case "knight":
-                return countAvaiableMovesForKnight(row, col, board);
-            case "bishop":
-                return countAvaiableMovesForBishop(row, col, board);
-            case "rook":
-                return countAvaiableMovesForRook(row, col, board);
-            case "queen":
-                return countAvaiableMovesForQueen(row, col, board);
-            case "pawn":
-                return countAvaiableMovesForPawn(row, col, board, moveNumber);
-            case "king":
-                return countAvaiableMovesForKing(row, col, board);
-            default:
-                return board;
+            case "knight": newBoard = countAvaiableMovesForKnight(row, col, board); break;
+            case "bishop": newBoard = countAvaiableMovesForBishop(row, col, board); break;
+            case "rook": newBoard = countAvaiableMovesForRook(row, col, board); break;
+            case "queen": newBoard = countAvaiableMovesForQueen(row, col, board); break;
+            case "pawn": newBoard = countAvaiableMovesForPawn(row, col, board, moveNumber); break;
+            case "king": newBoard = countAvaiableMovesForKing(row, col, board); break;
+            default: return board;
         }
+
+        newBoard[selectedIndex].possibleMoves = filterMovesLeavingKingInCheck(row, col, newBoard, pieceColour);
+        return newBoard;
     }
 }
 
--- kolosov.ivan/task-5/chess/src/utils/countAvailableMovesForKing.js
+++ kolosov.ivan/task-5/chess/src/utils/countAvailableMovesForKing.js
@@ -1,11 +1,14 @@
 function countAvailableMovesForKing(row, col, board) {
-    const theoreticallyPossibleMoves = [
+    let theoreticallyPossibleMoves = [
         {row: row + 1, col: col + 1}, {row: row - 1, col: col - 1},
         {row: row + 1, col: col - 1}, {row: row - 1, col: col + 1}, 
         {row: row, col: col + 1}, {row: row, col: col - 1},
         {row: row + 1, col: col}, {row: row - 1, col: col}
-        ];
-    const kingColour = board[row * 8 + col].pieceColour;
+    ];
+    
+    const kingIndex = row * 8 + col;
+
+    const kingColour = board[kingIndex].pieceColour;
     let availableMoves = [];
     for (let move of theoreticallyPossibleMoves) {
         if (move.row < 0 || move.row > 7 || move.col < 0 || move.col > 7){
@@ -16,6 +19,14 @@ function countAvailableMovesForKing(row, col, board) {
         }
         availableMoves.push(move);
     }
+
+    if (board[kingIndex].moveNumber === 0 && board[row * 8 + 7].moveNumber === 0 && board[row * 8 + 6].pieceType === "none" && board[row * 8 + 5].pieceType === "none") {
+        availableMoves.push({row: row, col: 6});
+    }
+    if (board[kingIndex].moveNumber === 0 && board[row * 8 + 0].moveNumber === 0 && board[row * 8 + 1].pieceType === "none" && board[row * 8 + 2].pieceType === "none" && board[row * 8 + 3].pieceType === "none") {
+        availableMoves.push({row: row, col: 2});
+    }
+
     board[row * 8 + col].possibleMoves = availableMoves;
     return board;
 }
